7e88e3fabac168ffd3dfef160591a81f72a43bb7
Pedro Oliveira <pedrovicboliveira@gmail.com>
2021-10-23 01:28:33 -0300

    test add new python repo

---
+++ TestaTkinter.py
@@ -0,0 +1,11 @@
+from tkinter import *
+window=Tk()
+btn=Button(window, text = "Este é um componente Botão", fg='blue')
+btn.place(x=80, y=100)
+lbl=Label(window, text= "Este é um componente de Rótulo", fg='red', font=("Helvetica", 16))
+lbl.place(x=60, y=50)
+txtfld=Entry(window, text="Este é um componente de Entrada de Texto", bd=5)
+txtfld.place(x=80, y=150)
+window.title('Bem-vindo ao Python')
+window.geometry("300x200+10+10")
+window.mainloop()
