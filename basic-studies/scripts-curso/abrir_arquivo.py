98ef137d7bb47cd206f3a3bdadbb27c111eb939c
pedrovbo <pedrovicboliveira@gmail.com>
2021-11-04 01:25:55 -0300

    renaming some files

--- basic-studies/scripts-curso/abrir_arquivo.py
+++ basic-studies/scripts-curso/abrir_arquivo.py
@@ -2,11 +2,11 @@ import os
 
 # Para o arquivo dados1.txt
 arquivo1 = open("dados1.txt")  # Caminho relativo
-arquivo2 = open("/basic-studies/scripts-curso/dados1.txt")  # Caminho absoluto
+arquivo2 = open("/home/pedrovbo/GitHub/repositorios/python/python-projects/basic-studies/scripts-curso/dados1.txt")  # Caminho absoluto
 
 # Para o arquivo dados2.txt
 arquivo3 = open("documentos/dados2.txt")  # Caminho relativo
-arquivo4 = open("/basic-studies/scripts-curso/documentos/dados2.txt")  # Caminho absoluto
+arquivo4 = open("/home/pedrovbo/GitHub/repositorios/python/python-projects/basic-studies/scripts-curso/documentos/dados2.txt")  # Caminho absoluto
 
 print(os.path.relpath(arquivo1.name))
 print(os.path.abspath(arquivo1.name))
