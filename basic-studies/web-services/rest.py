f23dd3bc2213f44d550dbf39690ffc21aa87625f
pedrovbo <pedrovicboliveira@gmail.com>
2021-11-21 23:33:09 -0300

    add some webservices study

--- basic-studies/web-services/rest.py
+++ basic-studies/web-services/rest.py
@@ -5,9 +5,6 @@ def consulta():
     response = requests.get('https://api.adviceslip.com/advice')
     print(response.status_code)
     print(response.json())
-    # for tip in response.json():
-    # print(tip['advice'])
-
 
 consulta()
 
