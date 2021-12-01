3db4bc2fe1786c2149ad2ab932fd347115e5fd12
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-01 13:21:41 -0300

    database connection and insert using python

--- basic-studies/database-with-python/connect.py
+++ basic-studies/database-with-python/connect.py
@@ -1,3 +1,31 @@
-import psycopg2
-conn = psycopg2.connect(user="postgres", password="123456", database="demo", host="localhost", port="5432")
-print("Successfully connected!")
+import psycopg2 as conector
+
+v1 = "5"
+v2 = "Geoffrey"
+v3 = "37"
+try:
+    # Abertura de conexão
+    conexao = conector.connect(user="postgres", password="123456", database="test", host="localhost", port="5432")
+
+    # Aquisição de um cursor
+    cursor = conexao.cursor()
+
+    # Execução comandos: SELECT..CREATE...
+    cursor.execute(
+        "INSERT INTO pessoa VALUES (%s, %s, %s);",
+        (v1, v2, v3)
+    )
+    # cursor.fetchall()
+
+    # Efetivação do comando
+    conexao.commit()
+
+except conector.DatabaseError as err:
+    print("Erro de banco de dados", err)
+
+finally:
+    # Fechamento das conexões
+    if conexao:
+
+        cursor.close()
+        conexao.close()
