b195ea4a871461678c3d8146488a343284b59181
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-12 14:24:29 -0300

    add routine optimization

--- basic-studies/convert-jpeg-to-pdf/convert-jpeg-pdf.py
+++ basic-studies/convert-jpeg-to-pdf/convert-jpeg-pdf.py
@@ -6,13 +6,13 @@ from fnmatch import fnmatch
 origem = input('Digite o caminho da pasta contendo os arquivos para serem convertidos: ')
 destino = input('Digite o caminho da pasta onde serão salvos os arquivos convertidos:')
 extensao = ".pdf"
-file_list = [file for file in os.listdir(origem) if fnmatch(file, '*.jpeg')]
+listaDeArquivos = [file for file in os.listdir(origem) if fnmatch(file, '*.jpeg')]
 contador = int(0)
 
-for i in file_list:
+for i in listaDeArquivos:
     arquivoConverter = origem + i
     image1 = Image.open(fr'{arquivoConverter}')
-    arquivoDestino = destino + "jpegConvertido" + contador.__str__() + extensao
+    arquivoDestino = destino + "arquivo Convertido " + contador.__str__() +" "+ extensao
     im1 = image1.convert('RGB')
     im1.save(fr'{arquivoDestino}')
     contador = contador + 1
