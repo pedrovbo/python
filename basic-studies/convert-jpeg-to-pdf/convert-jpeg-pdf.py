f3b4996312c8c499ce82216cfaf5098fa6ae1890
pedrovbo <pedrovicboliveira@gmail.com>
2021-12-12 12:54:49 -0300

    add data entry optimization

--- basic-studies/convert-jpeg-to-pdf/convert-jpeg-pdf.py
+++ basic-studies/convert-jpeg-to-pdf/convert-jpeg-pdf.py
@@ -1,5 +1,15 @@
 from PIL import Image
 
-image1 = Image.open(r'caminho-para-o-arquivo-origem')
-im1 = image1.convert('RGB')
-im1.save(r'caminho-para-o-arquivo-destino')
+leitura = input('Digite a quantidade de arquivos para converter:')
+quantidade = int(leitura)
+
+#TODO: implementar conversão em massa por extensão de arquivo
+for i in range(quantidade):
+    caminhoOrigem = input('Digite o caminho de origem:')
+    caminhoDestino = input('Digite o caminho de destino:')
+    image1 = Image.open(fr'{caminhoOrigem}')
+    im1 = image1.convert('RGB')
+    im1.save(fr'{caminhoDestino}')
+
+
+print("Fim do Programa")
